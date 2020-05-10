529c3bc5b859020a89d08a65eb59dcf3e0a69898
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2020-05-09 19:05:18 -0500

    Fixing errors

--- src/model/Tournament.java
+++ src/model/Tournament.java
@@ -95,18 +95,114 @@ public class Tournament {
 				while (actual.getNext() != null) {
 					actual = actual.getNext();
 				}
-				if (actual == first.getNext()) {
-					toAdd.setPrev(first);
-					first.setNext(toAdd);
-				}
-				else {
-					toAdd.setPrev(actual);
-					actual.setNext(toAdd);
-				}
+				toAdd.setPrev(actual);
+				actual.setNext(toAdd);
+
 			}
 		}
 	}
+
+	public void addLinkedListRecursive (long[] q, int index, LinkedListItem actual) {
+		if (index < q.length) {
+			LinkedListItem toAdd = new LinkedListItem(q[index]);
+			if (first == null) {
+				first = toAdd;
+			}
+			
+			else {
+				toAdd.setPrev(actual);
+				actual.setNext(toAdd);
+			}
+			addLinkedListRecursive (q, index + 1, toAdd);
+		}
+	}
 	
+	public boolean searchLinkedListIterative (long toSearch) {
+		boolean found = false;
+		LinkedListItem actual = first;
+		while (actual != null && !found) {
+			if (actual.getNumber() == toSearch) {
+				found = true;
+			}
+			else {
+				actual = actual.getNext();
+			}
+		}
+		return found;
+	}
+	
+	public boolean searchLinkedListRecursive (long toSearch, LinkedListItem actual) {
+		boolean found = false;
+		if (actual != null) {
+			if (actual.getNumber() == toSearch) {
+				found = true;
+			}
+			else {
+				found = searchLinkedListRecursive (toSearch, actual.getNext());
+			}
+		}
+		return found;
+	}
+	
+	public boolean removeLinkedListIterative (long toRemove) {
+		boolean removed = false;
+		LinkedListItem actual = first;
+		
+		while (actual != null && !removed) {
+			if (actual.getNumber() == toRemove) {
+				LinkedListItem prev = actual.getPrev();
+				LinkedListItem next = actual.getNext();
+				
+				//Preventing a nullPointer in first item
+				if (prev != null) {
+					prev.setNext(next);
+				}
+				//Preventing a nullPointer in last item
+				if (next != null) {
+					next.setPrev(prev);
+				}
+				//Setting new first
+				if (actual == first) {
+					first = actual.getNext();
+				}
+				removed = true;
+			}
+			else {
+				actual = actual.getNext();
+			}
+		}
+		return removed;
+	}
+	
+	public boolean removeLinkedListRecursive (long toRemove, LinkedListItem actual) {
+		boolean removed = false;
+		if (actual != null) {
+			if (actual.getNumber() == toRemove) {
+				LinkedListItem prev = actual.getPrev();
+				LinkedListItem next = actual.getNext();
+
+				//Preventing a nullPointer in first item
+				if (prev != null) {
+					prev.setNext(next);
+				}
+				//Preventing a nullPointer in last item
+				if (next != null) {
+					next.setPrev(prev);		
+				}
+				//Setting new first
+				if (actual == first) {
+					first = next;
+					first.setPrev(null);
+				}
+				removed = true;
+			}
+			else {
+				removed = searchLinkedListRecursive (toRemove, actual.getNext());
+			}
+		}
+		return removed;
+	}
+
 	
 	//Get Methods
 	public ArrayList<Long> getArrayList() {
--- tests/model/TournamentTest.java
+++ tests/model/TournamentTest.java
@@ -9,6 +9,10 @@ public class TournamentTest {
 	
 	private Tournament tournament;
 	
+	public TournamentTest () {
+		tournament = new Tournament();
+	}
+	
 	public long[] setup1() {
 		long[] n = new long[] {1,2,3,4,5,6,7,8,9,10}; 
 		return n;
@@ -16,7 +20,6 @@ public class TournamentTest {
 	
 	@Test
 	public void generateRandomNumbersTest() {
-		tournament = new Tournament();
 		long[] numbers = tournament.generateRandomNumbers(10);
 	}
 	
@@ -25,6 +28,97 @@ public class TournamentTest {
 		long[] n = setup1();
 		tournament = new Tournament();
 		tournament.addLinkedListIterative(n);
-		Assertions.assertEquals(tournament.getFirst().getNext().getNumber() , n[1]);
+		LinkedListItem actual = tournament.getFirst();
+		int i = 0;
+		while (actual != null) {
+			Assertions.assertEquals(actual.getNumber() , n[i]);
+			actual = actual.getNext();
+			i++;
+		}
+	}
+	
+	@Test
+	public void addLinkedListRecursiveTest () {
+		long[] n = setup1();
+		tournament.addLinkedListRecursive(n, 0, null);
+		LinkedListItem actual = tournament.getFirst();
+		System.out.println(actual);
+		
+		int i = 0;
+		while (actual != null) {
+			Assertions.assertEquals(actual.getNumber() , n[i]);
+			actual = actual.getNext();
+			i++;
+		}
+	}
+	
+	@Test
+	public void searchLinkedListIterativeTest () {
+		long[] n = setup1();
+		tournament.addLinkedListRecursive(n, 0, null);
+		LinkedListItem actual = tournament.getFirst();
+		
+		//Linked List 
+		Assertions.assertTrue(tournament.searchLinkedListIterative(1));
+		Assertions.assertTrue(tournament.searchLinkedListIterative(5));
+		Assertions.assertTrue(tournament.searchLinkedListIterative(10));
+		Assertions.assertFalse(tournament.searchLinkedListIterative(13));
+		
+	}
+	
+	@Test
+	public void searchLinkedListRecursiveTest () {
+		long[] n = setup1();
+		tournament.addLinkedListRecursive(n, 0, null);
+		LinkedListItem actual = tournament.getFirst();
+		
+		Assertions.assertTrue(tournament.searchLinkedListRecursive(1, actual));
+		Assertions.assertTrue(tournament.searchLinkedListRecursive(5, actual));
+		Assertions.assertTrue(tournament.searchLinkedListRecursive(10, actual));
+		Assertions.assertFalse(tournament.searchLinkedListRecursive(13, actual));
+	}
+	@Test
+	public void removeSearchLinkedListRecursiveTest () {
+		long[] n = setup1();
+		tournament.addLinkedListRecursive(n, 0, null);
+		LinkedListItem actual = tournament.getFirst();
+		
+		Assertions.assertFalse(tournament.searchLinkedListRecursive(13, actual));
+		Assertions.assertFalse(tournament.removeLinkedListRecursive(13, actual));
+		
+		Assertions.assertTrue(tournament.searchLinkedListRecursive(1, actual));
+		Assertions.assertTrue(tournament.removeLinkedListRecursive(1, actual));
+		Assertions.assertFalse(tournament.searchLinkedListRecursive(1, actual));
+		
+		Assertions.assertTrue(tournament.searchLinkedListRecursive(5, actual));
+		Assertions.assertTrue(tournament.removeLinkedListRecursive(5, actual));
+		Assertions.assertFalse(tournament.searchLinkedListRecursive(5, actual));
+		
+		Assertions.assertTrue(tournament.searchLinkedListRecursive(10, actual));
+		Assertions.assertTrue(tournament.removeLinkedListRecursive(10, actual));
+		Assertions.assertFalse(tournament.searchLinkedListRecursive(10, actual));
+	}
+	
+	@Test
+	public void removeSearchLinkedListIterativeTest () {
+		long[] n = setup1();
+		tournament.addLinkedListRecursive(n, 0, null);
+		LinkedListItem actual = tournament.getFirst();
+		
+		//Linked List
+		Assertions.assertTrue(tournament.searchLinkedListIterative(1));
+		Assertions.assertTrue(tournament.removeLinkedListIterative(1));
+		Assertions.assertFalse(tournament.searchLinkedListIterative(1));
+		
+		Assertions.assertTrue(tournament.searchLinkedListIterative(5));
+		Assertions.assertTrue(tournament.removeLinkedListIterative(5));
+		Assertions.assertFalse(tournament.searchLinkedListIterative(5));
+		
+		Assertions.assertTrue(tournament.searchLinkedListIterative(10));
+		Assertions.assertTrue(tournament.removeLinkedListIterative(10));
+		Assertions.assertFalse(tournament.searchLinkedListIterative(10));
+		
+		Assertions.assertFalse(tournament.removeLinkedListIterative(13));
+		Assertions.assertFalse(tournament.searchLinkedListIterative(13));
 	}
 }
